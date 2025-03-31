c8e337de3f915b1a0acc76c93e5b0b8d74081ea4
Toha8 <a.harbatovich@gmail.com>
2025-03-31 19:36:01 +0300

    Detekt homework done

--- detekt-rules/src/main/kotlin/ru/otus/detekt/TopLevelCoroutineInSuspendFunRule.kt
+++ detekt-rules/src/main/kotlin/ru/otus/detekt/TopLevelCoroutineInSuspendFunRule.kt
@@ -1,10 +1,25 @@
 package ru.otus.detekt
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.rules.fqNameOrNull
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.psiUtil.getCallNameExpression
+import org.jetbrains.kotlin.psi.psiUtil.hasSuspendModifier
+import org.jetbrains.kotlin.psi.psiUtil.parents
+import org.jetbrains.kotlin.resolve.calls.util.getType
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.typeUtil.supertypes
+
 
 class TopLevelCoroutineInSuspendFunRule(config: Config) : Rule(config) {
     override val issue: Issue = Issue(
@@ -13,4 +28,54 @@ class TopLevelCoroutineInSuspendFunRule(config: Config) : Rule(config) {
         description = "Avoid running top level coroutines inside suspend functions",
         debt = Debt.FIVE_MINS
     )
+
+    override fun visitCallExpression(expression: KtCallExpression) {
+        super.visitCallExpression(expression)
+
+        if (expression.containingFunction()?.modifierList?.hasSuspendModifier() == false) return
+
+        if (expression.getCallNameExpression()?.text in listOf("launch", "async")) {
+            var isLaunchedFromTopLevelCoroutine = false
+            var parent = expression.parent
+            while (parent != null) {
+                if (parent is KtDotQualifiedExpression) {
+                    val receiver = parent.receiverExpression
+                    val receiverType = parent.receiverExpression.getType(bindingContext)?.fqNameOrNull()
+                    //торое условие receiver.text == "viewModelScope" сделано так как по другому не находит
+                    //  receiver.getType(bindingContext)?.supertypes()?.any { it.fqNameOrNull() == FqName("kotlinx.coroutines.CoroutineScope") } == true
+                    // выдает null
+                    if (receiverType == FqName("kotlinx.coroutines.CoroutineScope") || receiver.text == "viewModelScope") {
+                        isLaunchedFromTopLevelCoroutine = true
+                    }
+                }
+                if (parent is KtCallExpression) {
+                    if (parent.getType(bindingContext)?.supertypes()?.any {
+                            it.fqNameOrNull() == FqName("kotlinx.coroutines.CoroutineScope")
+                        } == true
+                    ) {
+                        isLaunchedFromTopLevelCoroutine = true
+                    }
+                }
+
+                if (isLaunchedFromTopLevelCoroutine && parent is KtNamedFunction && parent.modifierList?.hasSuspendModifier() == true) {
+                    report(
+                        CodeSmell(
+                            issue = issue,
+                            entity = Entity.from(expression),
+                            message = "Avoid running top level coroutines inside suspend functions"
+                        )
+                    )
+                    return
+                }
+
+                parent = parent.parent
+            }
+        }
+
+    }
+
+    private fun KtCallExpression.containingFunction(): KtFunction? =
+        this.parents.filterIsInstance<KtFunction>().firstOrNull()
 }
+
+
---
+++ detekt-rules/src/test/kotlin/ru/otus/detekt/GlobalScopeRuleTest.kt
@@ -0,0 +1,92 @@
+package ru.otus.detekt
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import io.kotest.matchers.collections.shouldHaveSize
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.Test
+
+@KotlinCoreEnvironmentTest
+internal class GlobalScopeRuleTest(private val env: KotlinCoreEnvironment) {
+    private val rule = GlobalScopeRule(Config.empty)
+
+    @Test
+    fun `should report GlobalScope launch`() {
+        val code = """
+            import kotlinx.coroutines.GlobalScope
+            import kotlinx.coroutines.launch
+            
+            fun loadInfo() {
+                GlobalScope.launch { 
+                    
+                 }
+            }
+        """
+        val findings = rule.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 1
+    }
+
+    @Test
+    fun `should report GlobalScope async`() {
+        val code = """
+            import kotlinx.coroutines.GlobalScope
+            import kotlinx.coroutines.launch
+            
+            fun loadInfo() {
+                GlobalScope.async { 
+                    
+                 }
+            }
+        """
+        val findings = rule.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 1
+    }
+
+    @Test
+    fun `should not report 'val scope = GlobalScope'`() {
+        val code = """
+            import kotlinx.coroutines.CoroutineScope
+            import kotlinx.coroutines.GlobalScope
+            
+            fun loadInfo(scope: CoroutineScope) = Unit
+            
+            fun fetchData() {
+                val scope = GlobalScope
+                loadInfo(scope)
+            }
+        """
+        val findings = rule.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 0
+    }
+
+    @Test
+    fun `should not report CoroutineScope launch`() {
+        val code = """
+            import kotlinx.coroutines.CoroutineScope
+            import kotlinx.coroutines.launch
+            
+            fun example(scope: CoroutineScope) {
+                scope.launch { 
+                    println("This is a coroutine")
+                }
+            }
+        """.trimIndent()
+
+        val findings = rule.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 0
+    }
+
+    @Test
+    fun `should not report non-coroutine code`() {
+        val code = """
+            fun example() {
+                println("This is not a coroutine")
+            }
+        """.trimIndent()
+
+        val findings = rule.compileAndLintWithContext(env, code)
+        findings shouldHaveSize 0
+    }
+
+}
